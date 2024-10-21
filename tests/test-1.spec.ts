55a5d36cf9dcb8f0df3c9fce5027af102fca0b96
colin boardway <cboardway@crescendocollective.com>
2024-10-21 09:15:44 -0500

    swap websites to avoid bot filter

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,13 +1,12 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
-  await page.goto("https://www.amazon.com/");
-  await page.getByPlaceholder("Search Amazon").click();
-  await page.getByPlaceholder("Search Amazon").fill("nike shoes");
-  await page.getByRole("button", { name: "Go", exact: true }).click();
+  await page.goto("https://www.boardsource.xyz/");
+  await page.getByRole("navigation").getByText("Premium Boards").click();
+  await page.getByRole("link", { name: "Unicorne LP Our First V2" }).click();
 
   // wait 5 seconds for the page to load
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(3000);
 
   // log a message to the console
   console.log("Page loaded.");
